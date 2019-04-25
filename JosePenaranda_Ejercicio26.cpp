579c0fa36ab3e1dde936912c057a0b386f089bcf
Jose991019 <jd.penaranda@uniandes.edu.co>
2019-04-25 00:27:40 +0000

    commit

--- JosePenaranda_Ejercicio26.cpp
+++ JosePenaranda_Ejercicio26.cpp
@@ -4,13 +4,17 @@ using namespace std;
 
 
 void euler(float t_init, float t_end, float delta_t, float omega, string filename);
-//void runge_kutta(float t_init, float t_end, float delta_t, float omega, string filename);
+void RK4(float t_init, float t_end, float delta_t, float omega, string filename);
+float f(float y, float v, float x, float omega){
+    return -omega*omega*y;
+}
+
 
 
 int main(){
   float omega=1;
   euler(0.0, 30, 0.001/omega, omega, "euler_0001.dat");
-  //runge_kutta(0.0, 4.0/omega, 0.01/omega, omega, "implicit_001.dat");
+  RK4(0.0, 30, 0.001/omega, omega, "RK4_0001.dat");
   return 0;
 }
 
@@ -32,16 +36,28 @@ void euler(float t_init, float t_end, float delta_t, float omega, string filenam
   outfile.close();
 }
 
-//void runge_kutta(float t_init, float t_end, float delta_t, float omega, string filename){
-//  float t=t_init;
-//  float y=1.0;
-//  ofstream outfile;
-//  outfile.open(filename);
-//
-//  while(t<t_end){    
-//    outfile << t << " " << y << endl;
-//    y = y/(1+delta_t*omega);
-//    t = t + delta_t;
-//  }
-//  outfile.close();
-//}
+void RK4(float t_init, float t_end, float h, float omega, string filename){
+  float t=t_init;
+  float y=1.0;
+  float v = 0;
+  double k1, k2, k3, k4, l1, l2, l3, l4;
+  ofstream outfile;
+  outfile.open(filename);
+  outfile << t << " " << y << " " << v << endl;
+    
+  while(t<t_end){    
+    k1=h*v;
+    l1=h*f(y, v, t, omega);
+    k2=h*(v+l1/2);
+    l2=h*f(y+k1/2, v+l1/2, t+h/2, omega);
+    k3=h*(v+l2/2);
+    l3=h*f(y+k2/2, v+l2/2, t+h/2, omega);
+    k4=h*(v+l3);
+    l4=h*f(y+k3, v+l3, t+h, omega);
+    y+=(k1+2*k2+2*k3+k4)/6;
+    v+=(l1+2*l2+2*l3+l4)/6;
+    t += h;
+    outfile << t << " " << y << " " << v << endl;
+  }
+  outfile.close();
+}
--- grafica.py
+++ grafica.py
@@ -2,13 +2,32 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 datos1 = np.loadtxt('euler_0001.dat')
+datos2 = np.loadtxt('RK4_0001.dat')
 
 x1 = datos1[:,0]
 y1 = datos1[:,1]
 z1 = datos1[:,2]
 
-plt.figure(figsize = (10,10))
-plt.scatter(x1,y1,s = 0.4,label = 'Explicito')
+x2 = datos2[:,0]
+y2 = datos2[:,1]
+z2 = datos2[:,2]
 
-plt.legend()
+plt.figure(figsize = (10,10))
+
+plt.subplot(3,1,1)
+plt.scatter(x2,y2,s = 0.4)
+plt.xlabel('x')
+plt.ylabel('y')
+
+
+plt.subplot(3,1,2)
+plt.scatter(x2,z2,s = 0.4)
+plt.xlabel('x')
+plt.ylabel('dy/dx')
+
+plt.subplot(3,1,3)
+plt.scatter(y2,z2,s = 0.4)
+plt.xlabel('y')
+plt.ylabel('dy/dx')
+
 plt.savefig('Resultado.png')
